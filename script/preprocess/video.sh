23189fb0946b80ab19d96cbdfb4392129281f4cf
kimihiroh <kimihiro@andrew.cmu.edu>
2025-02-03 15:15:15 -0500

    update video.sh

--- script/preprocess/video.sh
+++ script/preprocess/video.sh
@@ -3,18 +3,21 @@
 eval "$(conda shell.bash hook)"
 conda activate bridge
 
+current_time=$(date "+%Y%m%d-%H%M%S")
+
 # preprocess videos
 filepath_input=./data/our-video-qa/preprocess/$1
 dirpath_original_video=/usr1/datasets/captain_cook_4d/gopro
 dirpath_output=/usr1/data/kimihiro/bridge/$2
 dirpath_log=./log/
 
-NUM_PROCESS=$(nproc)
+NUM_PROCESS=$(( $(nproc) / 2 ))
 
 ## == trim video == ##
 mkdir -p "$dirpath_output"/trimmed-videos/360p/
 mkdir -p "$dirpath_output"/trimmed-videos/2160p/
 video_list=()
+echo "Start video trimming: $(date)"
 while read -r item; do
     recording_id=$(echo "$item" | jq -r '.recording_id')
     example_id=$(echo "$item" | jq -r '.example_id')
@@ -31,7 +34,8 @@ while read -r item; do
     video_list+=("$end_time $filepath_input_2160 $filepath_output_2160")
 done < <(jq -c '.[]' "$filepath_input")
 
-parallel --eta --bar -j "$NUM_PROCESS" --colsep ' ' ffmpeg -ss 00:00:00 -to "{1}" -i "{2}" -c copy "{3}" >> $dirpath_log/trim.log 2>&1 ::: "${video_list[@]}"
+parallel -j "$NUM_PROCESS" --colsep ' ' ffmpeg -ss 00:00:00 -to "{1}" -i "{2}" -c copy "{3}" >> $dirpath_log/trim_"${current_time}".log 2>&1 ::: "${video_list[@]}"
+echo "End video trimming: $(date)"
 
 
 ## == sample w/&w/o resize == ##
@@ -39,6 +43,7 @@ mkdir -p "$dirpath_output"/sampled-frames/360p/
 mkdir -p "$dirpath_output"/sampled-frames/288p/
 mkdir -p "$dirpath_output"/sampled-frames/2160p/
 mkdir -p "$dirpath_output"/sampled-frames/765p/
+echo "Start image sampling/resizing: $(date)"
 triples=()
 for filepath in "$dirpath_output"/trimmed-videos/360p/*; do
     example_id=$(basename  "${filepath%.mp4}")
@@ -52,6 +57,7 @@ for filepath in "$dirpath_output"/trimmed-videos/2160p/*; do
     triples+=("$filepath 3840 $dirpath_output/sampled-frames/2160p/$example_id")
     mkdir -p "$dirpath_output"/sampled-frames/2160p/"$example_id"
     triples+=("$filepath 1360 $dirpath_output/sampled-frames/765p/$example_id")
-    mkdir -p "$example_id"/sampled-frames/765p/"$example_id"
+    mkdir -p "$dirpath_output"/sampled-frames/765p/"$example_id"
 done
-parallel --eta --bar -j "$NUM_PROCESS" --colsep ' ' ffmpeg -i "{1}" -vf "fps=1,scale={2}:-1" "{3}"/%d.png >> $dirpath_log/sample.log 2>&1 ::: "${triples[@]}"
+parallel -j "$NUM_PROCESS" --colsep ' ' ffmpeg -i "{1}" -vf "fps=1,scale={2}:-1" "{3}"/%d.png >> $dirpath_log/sample_"${current_time}".log 2>&1 ::: "${triples[@]}"
+echo "End image sampling/resizing: $(date)"
